da3f06fc5717d16e7a3f0f7b72c5aa91198b3fd9
Vaishvik Jaiswal <vaishvikjaiswal@Vaishviks-MacBook-Air.local>
2025-01-31 18:35:55 +0530

    phone

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Hero() {
     return (
-        <div className="text-white flex flex-col justify-start items-start text-left py-12 px-6 sm:px-12 ml-2 sm:ml-10">
+        <div className="text-white flex flex-row justify-start items-start gap-[15rem] text-left py-12 px-6 sm:px-12 ml-2 sm:ml-10">
             <div>
                 {/* Heading */}
                 <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold mt-4 w-full sm:w-[700px] mb-4">
@@ -15,19 +15,14 @@ function Hero() {
                     Sports, Entertainment, Economy or Finance.
                 </p>
 
-                {/* Call-to-Action Buttons */}
+                {/* Call-to-Action Buttons
                 <div className="flex flex-col sm:flex-row gap-6 mb-8">
-                    <Link href="/download">
-                        <button className="bg-white text-black px-6 py-3 rounded-md font-medium transition-all hover:bg-gray-400 w-full sm:w-auto">
-                            Download App
-                        </button>
-                    </Link>
                     <Link href="/trade">
                         <button className="bg-white text-black px-6 py-3 rounded-md font-medium transition-all hover:bg-gray-400 w-full sm:w-auto">
                             Trade Online
                         </button>
                     </Link>
-                </div>
+                </div> */}
 
                 {/* Disclaimer */}
                 <div className="flex items-center gap-2 text-sm text-gray-400">
@@ -37,6 +32,21 @@ function Hero() {
                         className="text-black"
                     />
                     <label htmlFor="age-check">For 18 years and above only</label>
+                </div>
+            </div> 
+            <div className="hidden lg:block flex justify-center items-center"> 
+                <div className="relative w-[320px] h-[640px] bg-white rounded-[40px] shadow-xl border border-gray-300 overflow-hidden">
+                    <div className="absolute top-0 left-1/2 -translate-x-1/2 w-24 h-6 bg-black rounded-b-lg"></div>  
+
+                    <video 
+                        className="w-full h-full object-cover"
+                        autoPlay
+                        muted
+                        loop 
+                        playsInline
+                    > 
+                        <source src="/assets/videos/testing.mp4" type="video/mp4" /> 
+                    </video> 
                 </div>
             </div>
         </div>
